662662e22fcef10d9b228ded3803c1dfa6026fe4
Yaroslav <ykuklevskiy@gmail.com>
2021-08-13 13:10:34 +0200

    Added 2nd lab

---
+++ MinSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Дана целочисленная матрица. Найти минимум из сумм элементов строк
+
+namespace MinSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                string row = Console.ReadLine();
+                matrix[i] = Array.ConvertAll(row.Split(' '), str => int.Parse(str));
+            }
+
+            long minSum = matrix[0].Sum();
+            foreach (int[] row in matrix)
+            {
+                long sum = row.Sum();
+                if(minSum > sum)
+                    minSum = sum;
+
+            }
+
+            Console.WriteLine(minSum);
+            Console.ReadKey();
+
+        }
+    }
+}
